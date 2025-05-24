8b5a6e1ca33466345b29b08b78a0f7907c310020
shankar-kruthiventi <shankar3336@gmail.com>
2025-05-24 20:47:45 +0530

    e-commerce initialization

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, () => console.log(`Listening at port ${port}`));
+}
+bootstrap().catch((error) => {
+  console.error('Error starting the application:', error);
+  process.exit(1);
+});
